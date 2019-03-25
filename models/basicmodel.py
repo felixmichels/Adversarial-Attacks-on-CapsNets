88b11dfe9db10344176c8cb3b2a08306a2c3122d
Felix Michels <felixm.michels@gmail.com>
2019-03-25 14:35:19 +0100

    Start abstract model super class

---
+++ models/basicmodel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 13:44:07 2019
+
+@author: felix
+"""
+
+import abc
+
+class BasicModel(object, metaclass=abc.ABCMeta):
+    
+    @property
+    @abc.abstractmethod
+    def probabilities(self):
+         raise NotImplementedError()
+         
+    @property
+    @abc.abstractmethod
+    def logits(self):
+        raise NotImplementedError()
+        
+    @property
+    @abc.abstractmethod
+    def prediction(self):
+        raise NotImplementedError()
