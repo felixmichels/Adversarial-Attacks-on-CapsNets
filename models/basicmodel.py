fef7328c38d0f32c6f24756aed09efbfba5134b5
Felix Michels <felixm.michels@gmail.com>
2019-04-01 21:06:45 +0200

    Style train.py
    Stype basicmodel
    
    Style lazy
    
    Style util
    
    Refactor code based on pep8 style recommendations

--- models/basicmodel.py
+++ models/basicmodel.py
@@ -10,53 +10,71 @@ import inspect
 import tensorflow as tf
 from util.lazy import lazy_scope_property
 
+
 class BasicModel(object):
-    
+    """
+    Base class for model objects.
+    Designed to work with lazy_scope_property;
+    thus decorated methods are initialized immediately and only once,
+    in an guaranteed order
+    """
+
     def __init__(self, img, label, trainable=True, scope=None):
+        """
+        img: Tensor, input images
+        label: Tensor, labels
+        trainable: Boolean, indicates if model is trainable
+        scope: Optional scope name for tensors in this model.
+               Uses the class name (of the subclass) as default
+        """
         self.img = img
         self.label = label
         self.trainable = trainable
 
         self.scope = scope or self.__class__.__name__
-        
+
         self.__initialize_properties()
-        
-        
-    @property
-    def name(self):
-        return self.scope
-        
+
     def __initialize_properties(self):
         # Set is_build = True, so that properties get added to respective lists
         self.is_build = True
         self.normal_vars = []
         self.training_vars = []
-        
-        #Fill normal_vars/training_vars
+
+        # Fill normal_vars/training_vars
         inspect.getmembers(self)
-        
-        # Set is_buidl = False, so that properties have normal behaviour
+
+        # Set is_build = False, so that properties have normal behaviour
         self.is_build = False
-        #Initialize properties
+        # Initialize properties
         with tf.variable_scope(self.scope):
             for v in self.normal_vars:
-                getattr(self,v)
-                
+                getattr(self, v)
+
             if self.trainable:
                 for v in self.training_vars:
-                    getattr(self,v)
-                    
+                    getattr(self, v)
+
+        # Var lists are not needed anymore,
+        # but MUST keep self.is_build for correct lazy_scope_property behaiour
         delattr(self, 'normal_vars')
         delattr(self, 'training_vars')
 
-            
     @property
     def name(self):
+        """
+        The name of the model.
+        May depend on hyperparameters and should be used
+        for save path names etc.
+        """
         return self.scope
-        
+
     @lazy_scope_property
     def train_placeholder(self):
+        """
+        Training placeholder for use with tf session feed_dict.
+        If the model is not trainable, returns a tf constant False
+        """
         if self.trainable:
             return tf.placeholder_with_default(True, shape=())
-        else:
-            return tf.constant(False)
+        return tf.constant(False)
--- train.py
+++ train.py
@@ -1,60 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 25 15:06:52 2019
-
 @author: felix
+Main script for training models based on the basicmodel class on cifar10
 """
 
-import tensorflow as tf
-import numpy as np
 import os
 import time
+import tensorflow as tf
+import numpy as np
 from util.config import cfg
 from util.util import load_cifar10, get_dir, get_model
 
+
 def train_epoch(sess, model, init, writer):
+    """Trains a single epoch and writes to summary (but doesn't save)"""
     sess.run(init)
     try:
         while True:
             _, loss, acc, step, summary = sess.run(
-                    [model.optimizer, model.loss, model.accuracy, tf.train.get_global_step(), summary_op],
-                    feed_dict={model.train_placeholder: True})
-                
-            tf.logging.log_every_n(tf.logging.INFO,
-                                   '| Steps: %5d | Loss: %5.3f | Accuracy: %1.3f',
-                                    cfg.train_log_every_n,
-                                    step, loss, acc)
-            
-            if step%cfg.summary_every_n==0:
+                [
+                    model.optimizer,
+                    model.loss,
+                    model.accuracy,
+                    tf.train.get_global_step(),
+                    summary_op
+                ],
+                feed_dict={model.train_placeholder: True})
+
+            tf.logging.log_every_n(
+                tf.logging.INFO,
+                '| Steps: %5d | Loss: %5.3f | Accuracy: %1.3f',
+                cfg.train_log_every_n,
+                step, loss, acc)
+
+            if step % cfg.summary_every_n == 0:
                 tf.logging.debug('Writing summary')
                 writer.add_summary(summary, global_step=step)
-            
+
     except tf.errors.OutOfRangeError:
         pass
 
+
 def test(sess, model, init, writer):
+    """Runs the test set and averages accuracy"""
     sess.run(init)
     acc_list = []
     try:
         while True:
-            acc_list.append(sess.run(model.accuracy, feed_dict={model.train_placeholder: False}))
+            acc_val = sess.run(model.accuracy,
+                               feed_dict={model.train_placeholder: False})
+            acc_list.append(acc_val)
     except tf.errors.OutOfRangeError:
         pass
     acc = np.average(acc_list)
     tf.logging.info('\nTest Accuracy: %1.3f', np.average(acc))
     summary = tf.Summary(value=[
-            tf.Summary.Value(tag=model.name+"/test_accuracy", simple_value=acc)])
+        tf.Summary.Value(tag=model.name+"/test_accuracy", simple_value=acc)])
     tf.logging.debug('Writing test summary')
     step = sess.run(tf.train.get_global_step())
     writer.add_summary(summary, global_step=step)
-    
+
+
 def train_with_test(sess, model, train_init, test_init, ckpt_dir, log_dir):
+    """Loads the model (if --reuse) and trains remaining epochs"""
     sess.run(tf.global_variables_initializer())
-    
+
     saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=5, keep_checkpoint_every_n_hours=2)
     writer = tf.summary.FileWriter(log_dir, sess.graph)
-    
+
     if cfg.restore:
         save_path = tf.train.latest_checkpoint(ckpt_dir)
         saver.restore(sess, save_path)
@@ -62,22 +76,22 @@ def train_with_test(sess, model, train_init, test_init, ckpt_dir, log_dir):
     last_safe_time = time.time()
     while sess.run(epoch) < cfg.epochs:
         ep_start_time = time.time()
-        
+
         ep = sess.run(epoch)
         tf.logging.info('Epoch: %d', ep)
         train_epoch(sess, model, train_init, writer)
         tf.logging.info('Time: %5.2f', time.time()-ep_start_time)
-                
-        if (ep+1)%cfg.test_every_n == 0:
+
+        if (ep+1) % cfg.test_every_n == 0:
             test(sess, model, test_init, writer)
-            
+
         sess.run(epoch_op)
 
-        if (ep+1)%cfg.save_every_n == 0 or time.time()-last_safe_time > cfg.save_freq:
+        if (ep+1) % cfg.save_every_n == 0 or time.time()-last_safe_time > cfg.save_freq:
             ckpt_path = os.path.join(ckpt_dir, 'model')
             saver.save(sess, ckpt_path, global_step=tf.train.get_global_step())
             last_safe_time = time.time()
-            
+
     test(sess, model, test_init, writer)
     writer.close()
 
@@ -86,52 +100,54 @@ summary_op = None
 epoch = None
 epoch_op = None
 
+
 def main(args):
     global summary_op, epoch, epoch_op
-    
+
     if cfg.debug:
         tf.logging.set_verbosity(tf.logging.DEBUG)
     else:
         tf.logging.set_verbosity(tf.logging.INFO)
-        
-    model_class = get_model(args[1])   
-    
+
+    model_class = get_model(args[1])
+
     with tf.variable_scope('data'):
         tf.logging.debug('Load data')
         train_data = load_cifar10(is_train=True, batch_size=cfg.batch_size)
-        test_data  = load_cifar10(is_train=False, batch_size=cfg.batch_size)
-        
-        iterator = tf.data.Iterator.from_structure(train_data.output_types, 
-                                               train_data.output_shapes)
+        test_data = load_cifar10(is_train=False, batch_size=cfg.batch_size)
+
+        iterator = tf.data.Iterator.from_structure(train_data.output_types,
+                                                   train_data.output_shapes)
         img, label = iterator.get_next()
-        
+
         tf.logging.debug('Creating iterator initializer')
         train_init = iterator.make_initializer(train_data)
-        test_init = iterator.make_initializer(test_data)	  
+        test_init = iterator.make_initializer(test_data)
 
     tf.train.create_global_step()
     epoch = tf.get_variable('epoch', dtype=tf.int32, initializer=0, trainable=False)
     epoch_op = tf.assign_add(epoch, 1)
     summary_op = tf.summary.merge_all()
-    
+
     tf.logging.debug('Creating model graph')
     model = model_class(img=img, label=label)
-    
+
     ckpt_dir = get_dir(cfg.ckpt_dir, model.name)
     log_dir = get_dir(cfg.log_dir, model.name)
-    
+
     tf.logging.debug('Creating summary op')
-    summary_op = tf.summary.merge_all() 
-    
+    summary_op = tf.summary.merge_all()
+
     if cfg.stop_before_session:
         exit()
-        
+
     tf.logging.debug('Starting session')
     with tf.Session() as sess:
         try:
             train_with_test(sess, model, train_init, test_init, ckpt_dir, log_dir)
         except KeyboardInterrupt:
             print("Manual interrupt occured")
+
 
 if __name__ == '__main__':
     tf.app.run()
