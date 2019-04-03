2c33842a627416935b923f70bf8b92461af4ed8b
Felix Michels <felixm.michels@gmail.com>
2019-04-03 15:05:25 +0200

    Add data augmentation functionality
    Including config hyperparameters indicating the strength of
    augmentation.
    
    Forgot data.py file
    
    Fix stuff

--- util/util.py
+++ util/util.py
@@ -8,43 +8,7 @@ import importlib
 import inspect
 import sys
 import tensorflow as tf
-import numpy as np
 from util.config import cfg
-
-
-def _load_scaled_cifar10():
-    if _load_scaled_cifar10.data is None:
-        (train_x, train_y), (test_x, test_y) = tf.keras.datasets.cifar10.load_data()
-
-        train_x = train_x.astype('float32')
-        train_x /= 255.0
-        train_y = train_y.squeeze().astype('int64')
-
-        test_x = test_x.astype('float32')
-        test_x /= 255.0
-        test_y = test_y.squeeze().astype('int64')
-
-        _load_scaled_cifar10.data = (train_x, train_y), (test_x, test_y)
-
-    return _load_scaled_cifar10.data
-
-
-_load_scaled_cifar10.data = None
-
-
-def load_cifar10(is_train=True, batch_size=None):
-    """
-    Returns a tf dataset with cifar10 images and labels
-    """
-    training, test = _load_scaled_cifar10()
-    x, y = training if is_train else test
-
-    dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    if is_train:
-        dataset = dataset.shuffle(4*batch_size if batch_size is not None else 1024)
-    if batch_size is not None:
-        dataset = dataset.batch(batch_size)
-    return dataset
 
 
 def get_dir(base, *args):
@@ -62,40 +26,6 @@ def get_dir(base, *args):
     return path
 
 
-def get_data(attack_name, n=None, override=False):
-    """
-    Loads original images from file, or creates file with random test images.
-    n: number of images. If image file already exists, None will load all.
-       If file doesn't exists yet, or override is True, n must not be None
-    override: If true, generate a new file
-    """
-    path = get_dir(cfg.data_dir, attack_name)
-    file_name = os.path.join(path, 'originals.npz')
-
-    if not os.path.isfile(file_name) or override:
-        _, (img, label) = _load_scaled_cifar10()
-
-        idx = np.random.permutation(len(img))
-        img = img[idx]
-        label = label[idx]
-
-        img = img[:n]
-        label = label[:n]
-
-        np.savez(file_name, img=img, label=label)
-
-    else:
-        tf.logging.debug('Loading from file %s', file_name)
-        with np.load(file_name) as npzfile:
-            img = npzfile['img']
-            label = npzfile['img']
-
-            img = img[:n]
-            label = img[:n]
-
-    return img, label
-
-
 def get_model(name):
     """
     Loads the model class (in cfg.model_pck) based on the file name
@@ -110,3 +40,22 @@ def get_model(name):
 
     tf.logging.debug('Found class %s', model_class)
     return model_class
+
+
+def func_with_prob(f, p):
+    """
+    Applies a function randomly
+
+    Parameters:
+        f: The function to apply
+        p: Probability, that the function should be applied
+
+    Returns:
+        A function, that returns for input x
+        f(x) with probability p,
+        or x with probability 1-p
+    """
+    def g(x):
+        choice = tf.random_uniform(shape=(), minval=0, maxval=1)
+        return tf.cond(choice < p, lambda: f(x), lambda: x)
+    return g
