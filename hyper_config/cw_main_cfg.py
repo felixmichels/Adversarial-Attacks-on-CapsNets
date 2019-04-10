177fefb00b5ad2b5f1d1f2cd7d10170a106eaccd
Felix Michels <felixm.michels@gmail.com>
2019-04-10 13:51:35 +0200

    Move cw initial c averaging to main module
    Also more documenting

--- hyper_config/cw_main_cfg.py
+++ hyper_config/cw_main_cfg.py
@@ -13,7 +13,7 @@ flags.DEFINE_string('attack_name', 'carlini_wagner', 'Name of the attack (used f
 flags.DEFINE_integer('number_img', 1000, 'Number of images to attack')
 
 flags.DEFINE_integer('max_opt_iter', 10000, 'Maximum number of stepf for adam optimizer')
-flags.DEFINE_integer('max_bin_iter', 6, 'Maximum number of steps in binary search')
+flags.DEFINE_integer('max_bin_iter', 5, 'Maximum number of steps in binary search')
 
 flags.DEFINE_float('rand_start_std', 0.0, 'Standard deviation of random start image')
-flags.DEFINE_float('kappa', 5.0, 'The confidence value kappa')
+flags.DEFINE_float('kappa', 1.0, 'The confidence value kappa')
