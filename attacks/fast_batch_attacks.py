1dc611f78da13f9fbac1711b14e6dc333fc45638
Felix Michels <felixm.michels@gmail.com>
2019-04-13 17:26:04 +0200

    Basic 'fast attack' module
    To be used with universal perturbations or adversarial training.

---
+++ attacks/fast_batch_attacks.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 15:30:24 2019
+
+@author: felix
+"""
+
+import tensorflow as tf
+from abc import ABC, abstractmethod
+from util.lazy import lazy_property
+
+class FastAttack(ABC):
+    
+    def __init__(self, model):
+        self.model = model
+        self.perturbation
+        
+    @lazy_property
+    def perturbation(self):
+        return tf.clip_by_value(self._unclipped_pert(),
+                                0 - self.model.img,
+                                1 - self.model.img)
+    
+    @abstractmethod
+    def _unclipped_pert(self):
+        pass
+    
+    
+class FGSM(FastAttack):
+    
+    def __init__(self, model, epsilon):
+        self.epsilon = epsilon
+        super(self.__class__, self).__init__(model)
+        
+        
+    def _unclipped_pert(self):
+        grad = tf.gradients(self.model.loss, self.model.img)[0]
+        return self.epsilon*tf.sign(grad)
